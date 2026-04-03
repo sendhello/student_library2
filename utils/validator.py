d4fabb0a5f46285408b729676192bf27fcd7679f
AudreyShelly3 <audreymhenyu26@gmail.com>
2026-04-03 17:31:20 +1100

    Update validator.py

--- utils/validator.py
+++ utils/validator.py
@@ -1,6 +1,15 @@
 """Validator module for validating library data."""
 class Validator:
     @staticmethod
+    def get_non_empty_string(prompt: str) -> str:
+        """Repeats input until a non-empty value is entered."""
+        while True:
+            value = input(prompt).strip()
+            if value:
+                return value
+            print("Invalid input. This field cannot be empty.")
+
+    @staticmethod
     def validate_member_id(member_id):
         """Validate member ID"""
         return str(member_id).strip() != ""
